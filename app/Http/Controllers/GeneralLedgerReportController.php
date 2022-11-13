791745272a0cf897c39d8f7e17a5c2790851e265
Alex Meier <92195211+coolerParty@users.noreply.github.com>
2022-11-13 22:17:16 +0800

    General Ledger Date Jev Filter Added.

--- app/Http/Controllers/GeneralLedgerReportController.php
+++ app/Http/Controllers/GeneralLedgerReportController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\AccountChart;
 use App\Models\BeginningBalance;
 use App\Models\JournalEntryVoucher;
+use Carbon\Carbon;
 use PDF;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -12,30 +13,56 @@ use Illuminate\Support\Facades\DB;
 
 class GeneralLedgerReportController extends Controller
 {
+    public function monthFormat($month)
+    {
+        if($month < 10)
+        {
+            return '0' . $month;
+        }
+        else
+        {
+            return $month;
+        }
+    }
+
     public function report($code, $year, $month)
     {
+        $monthFormatted = $this->monthFormat($month);
+
         $accounts = AccountChart::select('id','code','name')->where('code',$code)->first();
 
         if (empty($accounts)) {
             abort(404);
         }
-
+        // dd(Carbon::parse($year . $month . '-01' ));
         $journals = DB::table('journal_entry_vouchers')
             ->join('transactions', 'journal_entry_vouchers.id', '=', 'transactions.journal_entry_voucher_id')
             ->where('accountchart_id',$accounts->id)
+            ->whereBetween('jv_date',[
+                Carbon::parse($year . '-01-01'),
+                Carbon::parse($year . $monthFormatted . '-01' ),
+            ])
             ->selectRaw('type, accountchart_id,sum(debit) as subtotal_debit, sum(credit) as subtotal_credit, month(jv_date) as jv_month')
             ->groupBy('type','accountchart_id','jv_month')
             ->get();
 
         $jvAllMonths = $journals->pluck('jv_month','jv_month');
 
-        $beginningBalance = BeginningBalance::select('accountchart_id','amount')->where('accountchart_id')->first();
+        $beginningBalance = BeginningBalance::select('accountchart_id','amount')
+                            ->whereBetween('start_date',[
+                                    Carbon::parse($year . '-01-01'),
+                                    Carbon::parse($year . '-01-01' )->endOfYear(),
+                                ])
+                            ->where('accountchart_id')
+                            ->first();
 
          return view('report-form.general-ledger.general-ledger-report', compact('journals', 'year', 'month', 'beginningBalance','accounts','jvAllMonths'));
     }
 
     public function pdf($code, $year, $month)
     {
+        $monthFormatted = $this->monthFormat($month);
+
         $accounts = AccountChart::select('id','code','name')->where('code',$code)->first();
 
         if (empty($accounts)) {
@@ -45,13 +72,23 @@ class GeneralLedgerReportController extends Controller
         $journals = DB::table('journal_entry_vouchers')
             ->join('transactions', 'journal_entry_vouchers.id', '=', 'transactions.journal_entry_voucher_id')
             ->where('accountchart_id',$accounts->id)
+            ->whereBetween('jv_date',[
+                Carbon::parse($year . '-01-01'),
+                Carbon::parse($year . $monthFormatted . '-01' ),
+            ])
             ->selectRaw('type, accountchart_id,sum(debit) as subtotal_debit, sum(credit) as subtotal_credit, month(jv_date) as jv_month')
             ->groupBy('type','accountchart_id','jv_month')
             ->get();
 
         $jvAllMonths = $journals->pluck('jv_month','jv_month');
 
-        $beginningBalance = BeginningBalance::select('accountchart_id','amount')->where('accountchart_id')->first();
+        $beginningBalance = BeginningBalance::select('accountchart_id','amount')
+                            ->whereBetween('start_date',[
+                                    Carbon::parse($year . '-01-01'),
+                                    Carbon::parse($year . '-01-01' )->endOfYear(),
+                                ])
+                            ->where('accountchart_id')
+                            ->first();
 
         $pdf = PDF::loadView('report-form.general-ledger.general-ledger-report', compact('journals', 'year', 'month', 'beginningBalance','accounts','jvAllMonths'));
 
