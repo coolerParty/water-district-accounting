607b13536c1de606efbfad94a95b2b58ca792ace
Alex Meier <92195211+coolerParty@users.noreply.github.com>
2022-11-15 16:02:37 +0800

    Trial Balance Report Added.

--- app/Http/Controllers/FinancialStatementReportController.php
+++ app/Http/Controllers/FinancialStatementReportController.php
@@ -4,10 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\AccountChart;
 use App\Models\BeginningBalance;
-use App\Models\JournalEntryVoucher;
 use Carbon\Carbon;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use PDF;
 
 class FinancialStatementReportController extends Controller
 {
@@ -26,7 +25,7 @@ class FinancialStatementReportController extends Controller
 
 
         if ($type == 1) {
-            return $this->trialBalanceReport($year, $month);
+            return $this->trialBalancePDF($year, $month);
         }
     }
 
@@ -55,6 +54,34 @@ class FinancialStatementReportController extends Controller
         return view('report-form.financial-statement.trial-balance-report', compact('journals', 'year', 'month', 'beginningBalances', 'accounts'));
     }
 
+    public function trialBalancePDF($year, $month)
+    {
+        $monthFormatted = $this->monthFormat($month);
+        $accounts = AccountChart::select('id', 'code', 'name')->orderBy('code', 'ASC')->get();
+        $journals = DB::table('journal_entry_vouchers')
+            ->join('transactions', 'journal_entry_vouchers.id', '=', 'transactions.journal_entry_voucher_id')
+            ->whereBetween('jv_date', [
+                Carbon::parse($year . '-01-01')->startOfYear(),
+                Carbon::parse($year . '-' . $monthFormatted . '-01')->endOfMonth(),
+            ])
+            ->selectRaw('accountchart_id, sum(debit) as subtotal_debit, sum(credit) as subtotal_credit')
+            ->groupBy('accountchart_id')
+            ->whereRaw('(credit + debit) <> 0')
+            ->get();
+
+        $beginningBalances = BeginningBalance::select('accountchart_id', 'amount')
+            ->whereBetween('start_date', [
+                Carbon::parse($year . '-01-01')->startOfYear(),
+                Carbon::parse($year . '-01-01')->endOfYear(),
+            ])
+            ->get();
+
+        $pdf = PDF::loadView('report-form.financial-statement.trial-balance-report', compact('journals', 'year', 'month', 'beginningBalances', 'accounts'));
+        $filename = 'Trial Balance Report ' . $year . '-' . $month;
+        $pdf->setPaper('LEGAL', 'portrait');
+
+        return $pdf->download($filename . '.pdf');
+    }
 
     public function monthFormat($month)
     {
